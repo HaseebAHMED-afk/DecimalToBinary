2c75f7c5ef1e6e80ba8fd9112e4b10bbb71cbe93
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-05-16 21:15:17 +0500

    UI added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
+import TextField  from '@material-ui/core/TextField';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-     hello
+    <div className="input-section">
+      <TextField variant='filled'   color='secondary' label='Enter a number' className='input' />
+      <Button variant='contained' color='secondary' className='input'  >Check</Button>
     </div>
+    <div className="result">
+      
+    </div>
+   </div>
   );
 }
 
