7ce7d7aa5c43bd5483f03364413ad7a53e187abb
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-05-16 21:46:39 +0500

    Another UI bug

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ConverterFunction from './Utils/ConverterFunction'
 function App() {
 
   let [num , setNum] = useState<Number>(0)
-  let [result , setResult] = useState('')
+  let [result , setResult] = useState<any>(null)
 
 
   const handleChange = (e: any) =>{
@@ -16,20 +16,36 @@ function App() {
   }
 
   const handleClick = () =>{
-    console.log(ConverterFunction(num));
+    setResult(ConverterFunction(num));
   }
 
   return (
     <div className="App">
-
-    <div className="input-section">
-      <TextField variant='filled' style={{width:'100%'}} value={num} onChange={handleChange}  color='secondary' label='Enter a number' className='input' />
-      <Button variant='contained' color='secondary' className='input' onClick={handleClick}  >Convert</Button>
+      <div className="input-section">
+        <TextField
+          variant="filled"
+          style={{ width: "100%" }}
+          value={num}
+          onChange={handleChange}
+          color="secondary"
+          label="Enter a number"
+          className="input"
+        />
+        <Button
+          variant="contained"
+          color="secondary"
+          className="input"
+          onClick={handleClick}
+        >
+          Convert
+        </Button>
+      </div>
+      {result ? (
+        <div className="result">
+          <h1>{result}</h1>
+        </div>
+      ) : null}
     </div>
-    <div className="result">
-      <h1>1000000000000000000000000000000000000000000</h1>
-    </div>
-   </div>
   );
 }
 
